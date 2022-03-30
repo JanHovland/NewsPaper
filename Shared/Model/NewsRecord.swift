0f72aad6671b624b7ae6ff4dba4e16563ae42447
Jan Hovland <jan.hovland@lyse.net>
2022-03-30 19:50:24 +0200

    Update

---
+++ Shared/Model/NewsRecord.swift
@@ -0,0 +1,23 @@
+//
+//  NewsRecord.swift
+//  NewsPaper
+//
+//  Created by Jan Hovland on 30/03/2022.
+//
+
+import Foundation
+
+struct NewsRecord: Identifiable {
+    var id = UUID()
+    var status =  String()
+    var totalResults = Int()
+    var article_source_id : String?
+    var article_source_name = String()
+    var article_author : String?
+    var article_title  = String()
+    var article_description = String()
+    var article_url = String()
+    var article_urlToImage = String()
+    var article_publishedAt = String()
+    var article_content: String?
+}
